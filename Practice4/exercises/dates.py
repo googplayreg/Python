edf9f5837d243f26c129c78ba33e4f869f61caac
googplayreg <googplayreg@gmail.com>
2026-02-22 13:43:31 +0500

    editing the comments

--- Practice4/exercises/dates.py
+++ Practice4/exercises/dates.py
@@ -1,12 +1,12 @@
 import datetime as dt
 
-#1
+#1 subtract five days from current date
 cur_date = dt.datetime.now()
 new_date = cur_date - dt.timedelta(days=5)
 
 print(f"The date five day before: {new_date.date()}")
 
-#2
+#2 print yesterday, today, tomorrow
 today = dt.datetime.now()
 yesterday = today - dt.timedelta(days=1)
 tomorrow = today + dt.timedelta(days=1)
@@ -14,11 +14,11 @@ print(f"Yesterday: {yesterday.date()}")
 print(f"Today: {today.date()}")
 print(f"Tomorrow: {tomorrow.date()}")
 
-#3
+#3 drop microseconds from datetime
 no_micro = dt.datetime.now().replace(microsecond=0)
 print(f"Without microseconds: {no_micro}")
 
-#4
+#4 calculate two date difference in seconds
 date1 = dt.datetime(2026, 2, 25, 12, 0, 0)
 date2 = dt.datetime(2026, 2, 20, 18, 0, 0)
 diff = (date1 - date2).total_seconds()
--- Practice4/exercises/math.py
+++ Practice4/exercises/math.py
@@ -1,28 +1,28 @@
 import math
 
 #1 convert degree to radian
-# deg = int(input("Input degree: "))
-# rad = math.radians(deg)
-# print(f"Output radians: {rad}")
+deg = int(input("Input degree: "))
+rad = math.radians(deg)
+print(f"Output radians: {rad}")
 
-#2
-# h = int(input("Height: "))
-# b1 = int(input("Base, first value: "))
-# b2 = int(input("Base, second value: "))
-# trapezoid_area = float(((b1+b2)/2)*h)
-# print(trapezoid_area)
+#2 trapezoid area
+h = int(input("Height: "))
+b1 = int(input("Base, first value: "))
+b2 = int(input("Base, second value: "))
+trapezoid_area = float(((b1+b2)/2)*h)
+print(trapezoid_area)
 
-#3
-# n = int(input("Input number of sides: "))
-# l = int(input("Input the length of a side: "))
-# r_polygon_area = n * l * l / (4 * math.tan(math.pi/n))
-# # if r_polygon_area.is_integer():
-# #     result = int(r_polygon_area)
-# # else:
-# #     result = r_polygon_area
-# print(f"The area of a polygon is: {r_polygon_area}")
+#3 regular polygon area
+n = int(input("Input number of sides: "))
+l = int(input("Input the length of a side: "))
+r_polygon_area = n * l * l / (4 * math.tan(math.pi/n))
+# if r_polygon_area.is_integer():
+#     result = int(r_polygon_area)
+# else:
+#     result = r_polygon_area
+print(f"The area of a polygon is: {r_polygon_area}")
 
-#4
+#4 parallelogram area
 l = int(input("Length of base: "))
 h = int(input("Heigth of parallelogram: "))
 parallelogram_area = float(l * h)
