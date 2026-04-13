c56c02686083c6387d6c517874c766bfb80e12a2
googplayreg <googplayreg@gmail.com>
2026-04-13 20:16:46 +0500

    adding a comment (to procedures.sql)

--- Practice8/procedures.sql
+++ Practice8/procedures.sql
@@ -36,6 +36,11 @@ BEGIN
             VALUES (p_first_names[i], p_last_names[i], p_phones[i])
             ON CONFLICT (first_name, last_name) 
             DO UPDATE SET phone_number = EXCLUDED.phone_number;
+            -- Вариант с нормальной проверкой (RegEx)
+            -- IF p_phones[i] ~ '^\+?[0-9]{10,15}$' THEN
+            -- INSERT INTO phonebook(name, phone) VALUES(p_names[i], p_phones[i])
+            -- ON CONFLICT (phone) DO NOTHING;
+            -- Вставляем, если это цифры, возможно с + в начале, длиной от 10 до 15
         ELSE
             -- Если номер не прошел проверку, добавляем инфо в строку ошибок
             p_errors := p_errors || 'Ошибка в данных: ' || p_first_names[i] || ' ' || p_last_names[i] || ' (тел: ' || p_phones[i] || '); ';
